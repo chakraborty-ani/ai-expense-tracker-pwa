ee2659b3ebf36d50479019701b32da90145f876a
chakraborty-ani <anichakraborty20800@gmail.com>
2025-04-06 00:54:56 +0530

    Refactor NavUser component to use Redux for user details and remove hardcoded user data

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -23,11 +23,6 @@ import {
 } from "@/components/ui/sidebar"
 
 const data = {
-	user: {
-		name: "shadcn",
-		email: "m@example.com",
-		avatar: "/avatars/shadcn.jpg",
-	},
 	navMain: [
 		{
 			title: "Dashboard",
@@ -81,7 +76,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 				<NavMain items={data.navMain} />
 			</SidebarContent>
 			<SidebarFooter>
-				<NavUser user={data.user} />
+				<NavUser />
 			</SidebarFooter>
 		</Sidebar>
 	)
--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,11 +1,6 @@
 "use client"
 
-import {
-	IconDotsVertical,
-	IconLogout,
-	IconSettings,
-	IconUserCircle
-} from "@tabler/icons-react"
+import { IconDotsVertical, IconLogout, IconSettings, IconUserCircle } from "@tabler/icons-react"
 
 // COMPONENTS
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -22,17 +17,13 @@ import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "@/c
 
 // UTILS
 import { signOutCurrentUser } from "@/lib/firebase/firebase-config"
+import { useAppSelector } from "@/hooks/redux-hooks"
 
-export function NavUser({
-	user,
-}: {
-	user: {
-		name: string
-		email: string
-		avatar: string
-	}
-}) {
+export function NavUser() {
 	const { isMobile } = useSidebar()
+
+	// REDUX
+	const { currentUserDetails } = useAppSelector(state => state.user)
 
 	return (
 		<SidebarMenu>
@@ -44,12 +35,15 @@ export function NavUser({
 							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
 						>
 							<Avatar className="h-8 w-8 rounded-lg grayscale">
-								<AvatarImage src={user.avatar} alt={user.name} />
 								<AvatarFallback className="rounded-lg">CN</AvatarFallback>
 							</Avatar>
 							<div className="grid flex-1 text-left text-sm leading-tight">
-								<span className="truncate font-medium">{user.name}</span>
-								<span className="text-muted-foreground truncate text-xs">{user.email}</span>
+								<span className="truncate font-medium">
+									{currentUserDetails.firstname} {currentUserDetails.lastname}
+								</span>
+								<span className="text-muted-foreground truncate text-xs">
+									{currentUserDetails.email}
+								</span>
 							</div>
 							<IconDotsVertical className="ml-auto size-4" />
 						</SidebarMenuButton>
@@ -63,13 +57,14 @@ export function NavUser({
 						<DropdownMenuLabel className="p-0 font-normal">
 							<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 								<Avatar className="h-8 w-8 rounded-lg">
-									<AvatarImage src={user.avatar} alt={user.name} />
 									<AvatarFallback className="rounded-lg">CN</AvatarFallback>
 								</Avatar>
 								<div className="grid flex-1 text-left text-sm leading-tight">
-									<span className="truncate font-medium">{user.name}</span>
+									<span className="truncate font-medium">
+										{currentUserDetails.firstname} {currentUserDetails.lastname}
+									</span>
 									<span className="text-muted-foreground truncate text-xs">
-										{user.email}
+										{currentUserDetails.email}
 									</span>
 								</div>
 							</div>
